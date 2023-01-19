abcbad4af0055592932fa5d9503003784b5fcdd5
aksmf1442 <aksmf1442@naver.com>
2023-01-20 07:34:21 +0900

    [#13] feat: 홈으로 이동하는 버튼 및 기능 구현

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import SideBarButton from './sidebar/SideBarButton';
 import { useSetRecoilState } from 'recoil';
 import sideBarState from '../recoil/sideBarState';
+import Button from './common/Button';
 
 const HeaderContainer = styled.div`
 	display: flex;
@@ -16,7 +18,7 @@ const HeaderContainer = styled.div`
 
 	width: 100%;
 	height: 64px;
-	padding: 6rem 6rem 2rem 2rem;
+	padding: 2rem 6rem 2rem 2rem;
 
 	background: white;
 	box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.1);
@@ -29,17 +31,31 @@ const ContentBox = styled.div`
 	align-items: center;
 `;
 
+const ContentText = styled.span`
+	color: ${({ theme }) => theme.colors.LIGHT_BLACK};
+	font-size: 5rem;
+	font-weight: bold;
+`;
+
 function Header() {
+	const navigate = useNavigate();
 	const setSideBarOpen = useSetRecoilState(sideBarState);
 
 	const handleSideBarOpen = () => {
 		setSideBarOpen(prev => !prev);
 	};
 
+	const handleClickMainButton = () => {
+		navigate('');
+	};
+
 	return (
 		<HeaderContainer>
 			<ContentBox>
 				<SideBarButton handleSideBarOpen={handleSideBarOpen} />
+				<Button onClick={handleClickMainButton}>
+					<ContentText>일치</ContentText>
+				</Button>
 			</ContentBox>
 		</HeaderContainer>
 	);
