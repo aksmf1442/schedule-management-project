21551f5d01ccb683a5b05aa81e39a801d336e6b3
aksmf1442 <aksmf1442@naver.com>
2023-02-03 20:06:14 +0900

    [#38] feat: 백엔드 프로젝트 생성 및 초기 빌드

---
+++ backend/src/test/java/com/illch/IllchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
